90caf2b27ad639ca78bdf84d87cfaa745bc6c12a
Camilo Delvasto <camilodelvasto@gmail.com>
2017-11-20 14:38:22 -0500

    Section 1

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,54 +1,91 @@
 <template>
-  <section class="herox" v-bind:style="{ backgroundImage: `url('${imgsrc}')` }">
-    <div class="herox-body">
-      <div class="container">
-        <h1 class="title" v-html="headline"></h1>
-        <h3 class="tagline" v-html="tagline"></h3>
-        <button class="cta button is-success">Learn more</button>
+  <div>
+    <section class="herox">
+      <img class="hero-img" :src="imgsrc">
+      <div class="herox-body">
+        <div class="container">
+          <h1 class="title" v-html="headline"></h1>
+          <button class="cta button is-success" @click="toggleLipsum()" v-if="!showingLipsum">Learn more</button>
+          <button class="cta button is-success" @click="toggleLipsum()" v-if="showingLipsum">Hide info</button>
+          <transition name="fade">
+            <p class="lipsum" v-if="showingLipsum">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at diam eu nunc porttitor commodo. Vestibulum egestas vel eros sed posuere. 
+              Cras non condimentum mauris, ac luctus sem. Sed ex eros, ornare ut tincidunt et, consectetur et dui. Vestibulum non lacus eros. Nunc id fermentum leo. Praesent ultrices massa eu.
+            </p>
+          </transition>
+        </div>
       </div>
+    </section>
+    <div class="container">
+      <h3 class="tagline" v-html="tagline"></h3>
     </div>
-  </section>
+  </div>
 </template>
 
 <style scoped lang="scss">
 .herox {
-  background-size: cover;
-  position: relative;
-  height: 580px;
-  width: 1280px;
-  margin: 0 auto;
+  text-align: center;
+
+  .hero-img {
+    height: 444px;
+    width: 980px;
+    margin: 30px auto 0;
+  }
 
   .herox-body {
-    .container {
-      position: static;
-    }
 
     h1 {
       position: absolute;
-      top: 30px;
-      font-size: 60px;
+      top: -350px;
+      font-size: 48px;
       font-style: italic;
       font-weight: bold;
       color: $color-emphasis-alt;
+      text-align: left;
     }
 
     h3 {
       font-size: 24px;
-      position: absolute;
-      bottom: 20px;
     }
 
     .cta {
       position: absolute;
-      bottom: 120px;
+      left: 0;
+      top: -160px;
       font-size: 24px;
+    }
+
+    .lipsum {
+      background: $color-light-gray;
+      padding: 20px;
+      margin: 0 auto 20px;
+      text-align: left;
     }
   }
 }
+
+.fade-enter-active, .fade-leave-active {
+    transition: opacity .5s
+}
+.fade-enter, .fade-leave-to {
+    opacity: 0
+}
+
 </style>
 
 <script>
 export default {
-  props: [ 'headline', 'imgsrc', 'tagline' ]
+  props: [ 'headline', 'imgsrc', 'tagline' ],
+  data () {
+    return {
+      showingLipsum: false
+    }
+  },
+  methods: {
+    toggleLipsum () {
+      console.log(this.showingLipsum)
+      this.showingLipsum = !this.showingLipsum
+    }
+  }
 }
 </script>
